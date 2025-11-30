a3aa8a229872c5fdcecec17b6b599c9d04a5e820
badgateway <dooonkeypunch@gmail.com>
2025-11-30 21:20:51 +0700

    Initial Commit

---
+++ smartspends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  smartspends
+//
+//  Created by badgateway on 30.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ smartspends/smartspendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartspendsApp.swift
+//  smartspends
+//
+//  Created by badgateway on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct smartspendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
